1045088d76f550a31c9372dc8d67d8c41b32142e
StunnaDawg <jonsonallen9@gmail.com>
2023-03-01 14:09:37 -0400

    tomorrow variable update

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,25 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
 const footballAPIkey = 'caf956943f00c7484c8ee343fb5a56b22a6b7195aa7db3bc3ec6bb4d64097792'
 
 //{"country_id":"44","country_name":"England","league_id":"153","league_name":"Championship","league_season":"2022/2023"
 
 let today = new Date();
 let tomorrow = new Date();
-let tomorrowDate = tomorrow.setDate(tomorrow.getDate()+1);
+tomorrow.setDate(tomorrow.getDate()+3);
 var dd = String(today.getDate()).padStart(2, '0');
 var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
 var yyyy = today.getFullYear();
 
 today = yyyy + '-' + mm + '-' + dd
-tomorrowDate = yyyy + '-' + mm + '-' + dd
 
-function matches(team) {
-    fetch(`https://apiv3.apifootball.com/?action=get_events&from=${today}&to=${tomorrowDate}&league_id=153&APIkey=` + footballAPIkey)
+dd = String(tomorrow.getDate()).padStart(2, '0');
+mm = String(tomorrow.getMonth() + 1).padStart(2, '0');
+yyyy = tomorrow.getFullYear();
+
+tomorrow = yyyy + '-' + mm + '-' + dd
+
+function upcomingMatches(team) {
+    fetch( `https://apiv3.apifootball.com/?action=get_events&from=${today}&to=${tomorrow}&country_id=44&league_id=149&APIkey=` + footballAPIkey)
     .then(function(resp) {
         return resp.json()
 
 })
     .then(function(data) {
-        const matchData = data.map(item => {
+         const matchData = data.map(item => {
             return {
             matchDate: item.match_date,
             time: item.match_time,
@@ -31,12 +37,12 @@ function matches(team) {
             homeBadge: item.team_home_badge,
             awayBadge: item.away_home_badge,
             matchRef: item.match_referee,
-            leagueYear: item.leagueYear,
             //homeLineUp: item.lineup.home,
             //awayLineUp: item.lineup.away
         };
     });
         console.log('--->'+(JSON.stringify(matchData)));
+        displayUpcomingMatches(matchData);
     })
     
     .catch(function()  {
@@ -45,4 +51,11 @@ function matches(team) {
 
 //match_date, match_time, match_hometeam_id/match_awayteam_id, match_hometeam_name/match_awayteam_name, match_stadium, team_home/away_badge, match_referee, league_year
 //possibilities - lineup
-matches();
+upcomingMatches();
+
+//Display items 
+function displayUpcomingMatches(data) {
+
+}
+
+})//&from=${today}&to=2023-03-09&league_id=153
